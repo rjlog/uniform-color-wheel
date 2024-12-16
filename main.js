c1d08d3bfef43e0d2609f34cf751f44543b292ce
Loedje <loedje5@gmail.com>
2024-12-16 13:12:07 +0100

    setTexture()

--- main.js
+++ main.js
@@ -14,14 +14,16 @@ function main() {
   }
   gl.enable(gl.BLEND);
   gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+  const alignment = 1;
+  gl.pixelStorei(gl.UNPACK_ALIGNMENT, alignment);
   
   const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
   const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
   const program = createProgram(gl, vertexShader, fragmentShader);
   
-  initializePositions(program);
+  setPositions(program);
   const resolutionUniformLocation = gl.getUniformLocation(program, 'resolution');
-  
+  setTexture(program);
   
   gl.useProgram(program);
 
@@ -47,8 +49,7 @@ function drawScene(resolutionUniformLocation) {
   canvas.style.height = canvasHeight + 'px';
 }
 
-function initializePositions(program) {
-  const positionAttributeLocation = gl.getAttribLocation(program, 'position');
+function setPositions(program) {
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
@@ -61,13 +62,38 @@ function initializePositions(program) {
         0.0, 1.0,
       ]), gl.STATIC_DRAW);
   // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  const positionLocation = gl.getAttribLocation(program, 'position');
   const size = 2; // 2 components per iteration
   const type = gl.FLOAT;
   const normalize = false;
   const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
   let offset = 0; // start at the beginning of the buffer
-  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
-  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.vertexAttribPointer(positionLocation, size, type, normalize, stride, offset);
+  gl.enableVertexAttribArray(positionLocation);
+}
+
+function setTexture(program) {
+  gl.bindTexture(gl.TEXTURE_2D, gl.createTexture());
+  // fill texture with 3x1 pixels
+  const level = 0;
+  const internalFormat = gl.R32F;
+  const width = 3;
+  const height = 1;
+  const border = 0;
+  const format = gl.RED;
+  const type = gl.FLOAT;
+  const data = new Float32Array([0.0,  40.0, 80.0,]);
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border, format, type, data);
+
+  // set the filtering so we don't need mips and it's not filtered
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  
+  const textureLocation = gl.getUniformLocation(program, 'texture');
+  // Tell the shader to use texture unit 0 for u_texture
+  gl.uniform1i(textureLocation, 0);
 }
 
 function createProgram(gl, vertexShader, fragmentShader) {
